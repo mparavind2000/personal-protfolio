643f11357cd6c3f1384a3ce8c78f02c19ea9d2b1
purna aravind <mparavind2000@gmail.com>
2021-05-29 13:23:38 +0530

    initial commmiy

---
+++ main.js
@@ -0,0 +1,36 @@
+//      menu show y hidden
+const navMenu = document.getElementById('nav-menu');
+const navToggle= document.getElementById('nav-toggle');
+const navClose=document.getElementById('nav-close');
+
+
+
+//      menu show        
+//      valide if constant exists
+if(navToggle){
+    console.log("INSIDE IF");
+    navToggle.addEventListener('click',()=>{
+        console.log("INSIDE FUNTION");
+        navMenu.classList.add('show-menu')
+    })
+}
+//      menu hidden
+//      validdata if constant exists
+if(navClose){
+    navClose.addEventListener('click',()=>{
+    
+            navMenu.classList.remove('show-menu')
+    })
+}
+//      remove menu mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
